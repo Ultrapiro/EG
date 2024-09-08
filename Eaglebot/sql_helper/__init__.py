fe2635d59b14d82592509a61fbe1e70cbb85daa5
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-09-08 13:11:03 +0530

    Update __init__.py

--- Eaglebot/sql_helper/__init__.py
+++ Eaglebot/sql_helper/__init__.py
@@ -3,6 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+from telethon.sessions import StringSession
 
 # the secret configuration specific things
 from ..Config import Config
