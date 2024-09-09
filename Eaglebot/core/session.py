48a49a27adeb5c5a1ba2f94a2c966440e5c84d8e
Ultrapiro <wei18@shimmershade.tech>
2024-09-09 18:39:03 +0530

    Update session.py

--- Eaglebot/core/session.py
+++ Eaglebot/core/session.py
@@ -1,19 +1,10 @@
 import sys
 
-from telethon import TelegramClient
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 from telethon.sessions import StringSession
 
 from ..Config import Config
 from .client import EagleClient
-
-def validate_session(session):
-    if "==Eagle" and "bot==" in session.lower():
-        new_session = session[6:-5]
-        return str(new_session)
-    else:
-        print(f"EAGLEBOT SESSION - Wrong session string!")
-        sys.exit()
 
 __version__ = "1.10.6"
 
@@ -23,6 +14,7 @@ if Config.EAGLE_STRING:
     session = StringSession(str(Config.EAGLE_STRING))
 else:
     session = "EagleUserBot"
+
 try:
     eagle = EagleClient(
         session=session,
