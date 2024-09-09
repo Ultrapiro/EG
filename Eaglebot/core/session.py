2c2ed4511140b9514df14fe7caad4051158aebdf
Ultrapiro <wei18@shimmershade.tech>
2024-09-09 18:37:56 +0530

    Update session.py

--- Eaglebot/core/session.py
+++ Eaglebot/core/session.py
@@ -20,10 +20,9 @@ __version__ = "1.10.6"
 loop = None
 
 if Config.EAGLE_STRING:
-    StringSession(str(Config.EAGLE_STRING))
+    session = StringSession(str(Config.EAGLE_STRING))
 else:
     session = "EagleUserBot"
-
 try:
     eagle = EagleClient(
         session=session,
