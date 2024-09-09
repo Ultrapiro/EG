dc8726ef72a9045da5f7f2a3506073a289bbbf18
Ultrapiro <wei18@shimmershade.tech>
2024-09-09 22:21:37 +0530

    Update __main__.py

--- Eaglebot/__main__.py
+++ Eaglebot/__main__.py
@@ -14,22 +14,24 @@ from .utils import (
     load_plugins,
     setup_bot,
     startupmessage,
-    verifyLoggerGroup,)
-    
-LOGS = logging.getLogger("EAGLEUSERBOT")
+    verifyLoggerGroup,
+)
+
+LOGS = logging.getLogger("𝐸𝑎𝑔𝑙𝑒 𝑈𝑠𝑒𝑟𝑏𝑜𝑡")
 
 print(Eaglebot.__copyright__)
 print("𝐿𝑖𝑐𝑒𝑛𝑠𝑒𝑑 𝑈𝑛𝑑𝑒𝑟 𝑇ℎ𝑒 𝑇𝑒𝑟𝑚𝑠 𝑂𝑓 𝑇ℎ𝑒 " + Eaglebot.__license__)
+
 cmdhr = Config.HANDLER
 
+
 try:
-    LOGS.info("Starting Userbot")
+    LOGS.info("𝑆𝑡𝑟𝑎𝑖𝑛𝑔 𝐸𝑎𝑔𝑙𝑒 𝑈𝑠𝑒𝑟𝑏𝑜𝑡")
     eagle.loop.run_until_complete(setup_bot())
-    LOGS.info("TG Bot Startup Completed")
+    LOGS.info("𝑇𝑔 𝐵𝑜𝑡 𝑆𝑒𝑡𝑢𝑝 𝐶𝑜𝑚𝑝𝑙𝑒𝑡𝑒𝑑")
 except Exception as e:
     LOGS.error(f"{e}")
     sys.exit()
-    
 async def startup_process():
     try:
         await verifyLoggerGroup()
@@ -37,36 +39,35 @@ async def startup_process():
         await load_plugins("assistant")
         await externalrepo()
         await killer()
-        LOGS.info(
-        "============================================================================"
-    )
-        LOGS.info("||               Yay your userbot is officially working.!!!")
-        LOGS.info(
-        f"||   Congratulation, now type {cmdhr}alive to see message if eagle is live"
-    )
-        LOGS.info("||   If you need assistance, head to https://t.me/catuserbot_support")
-        LOGS.info(
-        "============================================================================"
-    )
+        print("----------------")
+        print("Starting Bot Mode!")
+        print("⚜ LegendBot Has Been Deployed Successfully ⚜")
+        print("OWNER - @LegendBoy_XD")
+        print("Group - @LegendBot_XD")
+        print("----------------")
         await verifyLoggerGroup()
         await add_bot_to_logger_group(BOTLOG_CHATID)
         if PM_LOGGER_GROUP_ID != -100:
             await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
-            await startupmessage()
-            return
-            
-            async def externalrepo():
-                string = "<b>Your external repo plugins have imported.<b>\n\n"
-                if Config.EXTERNAL_REPO:
-                    data = await install_externalrepo(
-                        Config.EXTERNAL_REPO, Config.EXTERNAL_REPOBRANCH, "xtraplugins"
+        await startupmessage()
+        await hekp()
+    except Exception as e:
+        LOGS.error(f"{str(e)}")
+        sys.exit()
+
+
+async def externalrepo():
+    string = "<b>Your external repo plugins have imported.<b>\n\n"
+    if Config.EXTERNAL_REPO:
+        data = await install_externalrepo(
+            Config.EXTERNAL_REPO, Config.EXTERNAL_REPOBRANCH, "xtraplugins"
         )
-                    string += f"<b>➜ Repo:  </b><a href='{data[0]}'><b>{data[1]}</b></a>\n<b>     • Imported Plugins:</b>  <code>{data[2]}</code>\n<b>     • Failed to Import:</b>  <code>{', '.join(data[3])}</code>\n\n"
-                    if Config.VCMODE:
-                        data = await install_externalrepo(Config.VC_REPO, Config.VC_REPOBRANCH, "catvc")
-                        string += f"<b>➜ Repo:  </b><a href='{data[0]}'><b>{data[1]}</b></a>\n<b>     • Imported Plugins:</b>  <code>{data[2]}</code>\n<b>     • Failed to Import:</b>  <code>{', '.join(data[3])}</code>\n\n"
-                        if "Imported Plugins" in string:
-                            await eagle.tgbot.send_message(BOTLOG_CHATID, string, parse_mode="html")
+        string += f"<b>➜ Repo:  </b><a href='{data[0]}'><b>{data[1]}</b></a>\n<b>     • Imported Plugins:</b>  <code>{data[2]}</code>\n<b>     • Failed to Import:</b>  <code>{', '.join(data[3])}</code>\n\n"
+            if Config.VCMODE:
+        data = await install_externalrepo(Config.VC_REPO, Config.VC_REPOBRANCH, "catvc")
+        string += f"<b>➜ Repo:  </b><a href='{data[0]}'><b>{data[1]}</b></a>\n<b>     • Imported Plugins:</b>  <code>{data[2]}</code>\n<b>     • Failed to Import:</b>  <code>{', '.join(data[3])}</code>\n\n"
+    if "Imported Plugins" in string:
+        await eagle.tgbot.send_message(BOTLOG_CHATID, string, parse_mode="html")
 
 
 eagle.loop.run_until_complete(startup_process())
