7887ca82685e5f357a950d7caad93b2a47a8af38
Ultrapiro <wei18@shimmershade.tech>
2024-09-09 18:29:34 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -175,7 +175,6 @@ class Config(object):
     EXTERNAL_REPOBRANCH = os.environ.get("EXTERNAL_REPOBRANCH", "Bad")
     # for vc plugins
     VCMODE = os.environ.get("VCMODE", True)
-    if bool(VCMODE and (VCMODE.lower() != "Fasle"))
     VC_SESSION = os.environ.get("VC_SESSION", None)
     VC_REPO = os.environ.get("VC_REPO", "https://github.com/badmunda98/EagleVc")
     VC_REPOBRANCH = os.environ.get("VC_REPOBRANCH", "Bad")
